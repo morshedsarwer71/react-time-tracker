8b1935b14fb06c7484c75b430db1fe4243cfd24b
Morshed Sarwer <morshedsarwer@gmail.com>
2021-02-18 02:30:23 +0600

    toggle reminder conditionl stylong

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,9 +2,9 @@ import { FaTimes } from 'react-icons/fa'
 
 import React from 'react'
 
-const Task = ({task,onDelete}) => {
+const Task = ({task,onDelete,onToggle}) => {
     return (
-        <div className='task'>
+        <div className='task' onDoubleClick={ () => onToggle(task.id) }>
             <h3>
                 {task.text}
                 <FaTimes style={{color:'red', cursor:'pointer'}} 
