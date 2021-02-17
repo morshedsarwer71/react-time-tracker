ef06fac811cac89e11bcd3d880c2caa91c6dc647
Morshed Sarwer <morshedsarwer@gmail.com>
2021-02-17 15:20:49 +0600

    component and props developed

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+
+const Header = ({title}) => {
+    return (
+        <header>{title}</header>
+    )
+}
+
+Header.defaultProps={
+    title:'Task Tracker'
+}
+Header.propTypes={
+    title: PropTypes.string.isRequired
+}
+export default Header
