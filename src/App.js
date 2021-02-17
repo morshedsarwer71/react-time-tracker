094d98ff24d436cdb4c65674e07df2c54b6150cc
Morshed Sarwer <morshedsarwer@gmail.com>
2021-02-17 20:43:50 +0600

    global state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 function App() {
+
+  const [tasks, setState] = useState([
+    {
+        id:1,
+        text:'Meeting with docktor',
+        day:'feb 6th 1:30pm',
+        reminder:true
+    },
+    {
+        id:2,
+        text:'Meeting school',
+        day:'feb 6th 2:30pm',
+        reminder:true
+    },
+    {
+        id:3,
+        text:'Food shooping',
+        day:'feb 6th 3:30pm',
+        reminder:false
+    }
+])
+
   return (
     <div className="container">
      <Header />
-     <Tasks />
+     <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,5 @@
-import { useState } from 'react'
 
-
-const Tasks = () => {
-
-    //state and useState hook
-
-    const [tasks, setState] = useState([
-        {
-            id:1,
-            text:'Meeting with docktor',
-            day:'feb 6th 1:30pm',
-            reminder:true
-        },
-        {
-            id:2,
-            text:'Meeting school',
-            day:'feb 6th 2:30pm',
-            reminder:true
-        },
-        {
-            id:3,
-            text:'food shooping',
-            day:'feb 6th 3:30pm',
-            reminder:false
-        }
-    ])
+const Tasks = ({tasks}) => {
 
     return (
         <>
