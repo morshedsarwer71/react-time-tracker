2330535fab31565629f5fe1efbd779a4416b2d24
Morshed Sarwer <morshedsarwer@gmail.com>
2021-02-19 15:06:06 +0600

    axios use in this project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddForm from './components/AddTask'
+import axios from 'axios';
 function App() {
   
   const [showAddTask, setShowTask ]=useState(false);
-  const [tasks, setTasks] = useState([
-    {
-        id:1,
-        text:'Meeting with docktor',
-        day:'feb 6th 1:30pm',
-        reminder:true
-    },
-    {
-        id:2,
-        text:'Meeting school',
-        day:'feb 6th 2:30pm',
-        reminder:true
-    },
-    {
-        id:3,
-        text:'Food shooping',
-        day:'feb 6th 3:30pm',
-        reminder:false
-    }
-])
+  const [tasks, setTasks] = useState([])
 
 const deleteTask = (id) => {
+axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+.then(res=>{
+  const result = res.data;
+  console.log('deleted');
+})
   setTasks(tasks.filter((task) => task.id!==id));
 }
 
@@ -35,14 +22,34 @@ const toggleReminder=(id)=>{
 }
 
 const addTask = (task)=>{
-  const id = Math.floor(Math.random() * 1000000)+1;
-  const newTask = {id,...task}
-  setTasks([...tasks, newTask]);
+  // const id = Math.floor(Math.random() * 1000000)+1;
+  // const newTask = {id,...task}
+  // setTasks([...tasks, newTask]);
+  setTasks();
 }
 
 const addButtonToogle=()=>{
   setShowTask(!showAddTask)
 }
+
+useEffect(()=>{
+  const fetchData = async ()=>{
+
+    axios.get(`https://jsonplaceholder.typicode.com/posts`)
+    .then(res=>{
+      const data = res.data;
+      // console.log(data);
+      setTasks(data);
+    });
+    // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // const data = await res.json()
+    // setTasks(data);
+    //console.log(data[0]);
+  }
+
+  fetchData();
+},[]);
+
   return (
     <div className="container">
      <Header addOn={addButtonToogle}/>
